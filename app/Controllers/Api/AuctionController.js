f5d5e7cce457a8010797f1d8b297178a6e457db7
Lavish2001 <lneoliya91@gmail.com>
2023-04-11 11:30:24 +0530

    changes done at last stage

--- app/Controllers/Api/AuctionController.js
+++ app/Controllers/Api/AuctionController.js
@@ -1,15 +1,113 @@
-const { User } = model("");
-const { Item } = model("");
-const { Chat } = model("");
-const { Auction } = model("");
-const { Bid } = model("");
+const { User, Item, Chat, Auction, Bid } = model("");
 const { checkAdmin } = helper("Helper");
 const { Op } = require("sequelize");
-const pdf = require('pdfkit');
 const fs = require('fs');
+const pdf = require('pdfkit');
 const path = require('path');
 const pdfSavePath = path.join(__dirname, '../../../Public/Pdf');
 const pdfImagePath = path.join(__dirname, '../../../Public/Items');
+
+const createPdf = async (id) => {
+
+    const clear = setInterval(async () => {
+        const auction = await Auction.findOne({ where: { id: id } });
+        if (auction.ended_at <= Date.now()) {
+            try {
+                clearInterval(clear);
+                const auction = await Auction.findOne({
+                    where: { [Op.and]: { id: id, bidder_id: { [Op.ne]: null } } }, include: [{
+                        model: Item, as: 'Item', attributes: { exclude: ['createdAt', 'updatedAt'] }
+                    },
+                    {
+                        model: Bid, as: 'Bidding', order: [['bid_amount', 'DESC']], limit: 1, attributes: { exclude: ['createdAt', 'updatedAt'] }, include: {
+                            model: User, as: 'Bidder', attributes: { exclude: ['createdAt', 'updatedAt', 'password', 'email', 'admin'] }
+                        }
+                    }]
+                });
+
+                if (auction) {
+                    const myPath = path.join(pdfSavePath, `${auction.id}.pdf`);
+
+                    const doc = new pdf();
+                    // const create = new Table();
+                    doc.pipe(fs.createWriteStream(myPath));
+
+                    // pdf heading
+
+                    doc.fontSize(14).text('Auction Result', { align: 'center', underline: true }).moveDown(2);
+
+
+                    // Pdf Item Image
+
+                    const imageWidth = 180;
+                    const imageHeight = 150;
+                    const x = (doc.page.width - imageWidth) / 2;
+                    const y = doc.y;
+                    doc.image(`${pdfImagePath}/${auction.Item.image}`, {
+                        width: imageWidth,
+                        height: imageHeight,
+                        align: 'center',
+                        valign: 'center',
+                        cover: false,
+                        x: x,
+                        y: y,
+                    })
+                        .moveDown(2);
+
+                    doc.fontSize(10).text(`Item Name: ${auction.Item.item_name}`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Description: ${auction.Item.description}`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Base Price: ${auction.Item.base_price}`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Highest Bid: ${auction.Bidding.length ? auction.Bidding[0].bid_amount : 0} $`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Sold To: ${auction.Bidding.length ? auction.Bidding[0].Bidder.username : `Unsold`}`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Started At: ${new Date(auction.started_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`, { align: 'center' }).moveDown();
+                    doc.fontSize(10).text(`Closes At: ${new Date(auction.ended_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`, { align: 'center' }).moveDown();
+
+                    doc.addPage();
+
+                    const details = await Bid.findAll({
+                        where: { auction_id: id }, attributes: { exclude: ['createdAt', 'updatedAt', 'auction_id'] }, include: [{
+                            model: User, as: 'Bidder', attributes: { exclude: ['createdAt', 'updatedAt', 'password', 'email', 'admin'] }
+                        }]
+                    });
+
+                    doc.text('Auction Biddings', { align: 'center' }).moveDown(2);
+
+                    const table = new (require('voilab-pdf-table'))(doc, { bottomMargin: 30 });
+                    table
+                        // add some plugins (here, a 'fit-to-width' for a column)
+                        .addPlugin(new (require('voilab-pdf-table/plugins/fitcolumn'))({
+                            column: 'name'
+                        })).setColumnsDefaults({
+                            align: 'left'
+                        })
+                        .addColumns([{ id: 'name', header: 'Name', align: 'left' }, { id: 'amount', header: 'Amount', align: 'left' }])
+                    const tableData = details.map((detail) => {
+                        return {
+                            name: detail.Bidder.username,
+                            amount: detail.bid_amount
+                        };
+                    });
+
+                    table.addBody(tableData);
+                    doc.moveDown();
+
+                    doc.end();
+
+                    // Wait for the PDF to finish writing before sending the response
+                    await new Promise(resolve => {
+                        doc.on('end', resolve);
+                    });
+
+                    return console.log('Pdf Created Successfully');
+                } else {
+                    return console.log('ERROR');
+                }
+            } catch (err) {
+                return console.log(err.message);
+            }
+        }
+    }, 1000);
+}
 
 
 
@@ -34,8 +132,9 @@ module.exports = class AuctionController {
                         if (find.ended_at < Date.now()) {
                             await find.update({
                                 started_at: Date.now(),
-                                ended_at: Date.now() + 10000
+                                ended_at: Date.now() + 30000
                             });
+                            createPdf(find.id);
                             return res.status(200).json({ 'status': 'success', 'message': 'Auction Started and ends in next 30 minutes' })
                         } else {
                             return res.status(400).json({ 'status': 'failed', 'message': 'Item Already in Live Auction' })
@@ -44,12 +143,13 @@ module.exports = class AuctionController {
                         return res.status(400).json({ 'status': 'failed', 'message': 'Item Already Sold Out' })
                     }
                 } else {
-                    await Auction.create({
+                    const auction = await Auction.create({
                         item_id: findItem.id,
                         bid_amount: findItem.base_price,
                         started_at: Date.now(),
-                        ended_at: Date.now() + 10000
+                        ended_at: Date.now() + 30000
                     });
+                    createPdf(auction.id);
                     return res.status(200).json({ 'status': 'success', 'message': 'Auction Started and ends in next 30 minutes' })
                 }
             } else {
@@ -107,11 +207,11 @@ module.exports = class AuctionController {
         try {
             const { id, bid } = req.body;
             const auction = await Auction.findOne({ where: { [Op.and]: { id: id, ended_at: { [Op.gt]: Date.now() } } } });
-            console.log(auction)
             if (Number(bid) <= Number(auction.bid_amount)) {
                 return res.status(400).json({ 'status': 'failed', 'message': 'Increase Bid Amount' })
             } else {
                 const checkHignestBid = await Bid.findOne({
+                    where: { auction_id: id },
                     order: [['bid_amount', 'DESC']]
                 });
                 if (checkHignestBid && req.user.id === checkHignestBid.bidder_id) {
@@ -155,7 +255,6 @@ module.exports = class AuctionController {
     // GET SINGLE AUCTION ITEM //
 
     async getAuctionItemSingle(req, res) {
-        console.log(req.query)
         try {
             const { id } = req.query;
             const auction_item = await Auction.findOne({
@@ -223,32 +322,70 @@ module.exports = class AuctionController {
             });
 
             if (auction) {
-                const myPath = path.join(pdfSavePath, `${auction.id}-${Date.now()}.pdf`)
+                const myPath = path.join(pdfSavePath, `${auction.id}.pdf`);
 
                 const doc = new pdf();
+                // const create = new Table();
                 doc.pipe(fs.createWriteStream(myPath));
 
                 // pdf heading
 
-                doc.fontSize(16).text('Auction Result', { align: 'center', underline: true }).moveDown(2);
+                doc.fontSize(14).text('Auction Result', { align: 'center', underline: true }).moveDown(2);
 
-                // pdf item image
 
+                // Pdf Item Image
+
+                const imageWidth = 180;
+                const imageHeight = 150;
+                const x = (doc.page.width - imageWidth) / 2;
+                const y = doc.y;
                 doc.image(`${pdfImagePath}/${auction.Item.image}`, {
-                    width: 300,
-                    height: 300,
+                    width: imageWidth,
+                    height: imageHeight,
                     align: 'center',
                     valign: 'center',
-                    fit: [250, 300]
-                }).moveDown(2);
+                    cover: false,
+                    x: x,
+                    y: y,
+                })
+                    .moveDown(2);
 
-                doc.fontSize(12).text(`Item Name: ${auction.Item.item_name}`).moveDown();
-                doc.fontSize(12).text(`Description: ${auction.Item.description}`).moveDown();
-                doc.fontSize(12).text(`Base Price: ${auction.Item.base_price}`).moveDown();
-                doc.fontSize(12).text(`Highest Bid: ${auction.Bidding.length ? auction.Bidding[0].bid_amount : 0}$`).moveDown();
-                doc.fontSize(12).text(`Sold To: ${auction.Bidding.length ? auction.Bidding[0].Bidder.username : `Unsold`}`).moveDown();
-                doc.fontSize(12).text(`Started At: ${new Date(auction.started_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`).moveDown();
-                doc.fontSize(12).text(`Closes At: ${new Date(auction.ended_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`).moveDown();
+                doc.fontSize(10).text(`Item Name: ${auction.Item.item_name}`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Description: ${auction.Item.description}`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Base Price: ${auction.Item.base_price}`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Highest Bid: ${auction.Bidding.length ? auction.Bidding[0].bid_amount : 0} $`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Sold To: ${auction.Bidding.length ? auction.Bidding[0].Bidder.username : `Unsold`}`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Started At: ${new Date(auction.started_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`, { align: 'center' }).moveDown();
+                doc.fontSize(10).text(`Closes At: ${new Date(auction.ended_at).toLocaleTimeString([], { hour12: true, hour: 'numeric', minute: 'numeric' })}`, { align: 'center' }).moveDown();
+
+                doc.addPage();
+
+                const details = await Bid.findAll({
+                    where: { auction_id: id }, attributes: { exclude: ['createdAt', 'updatedAt', 'auction_id'] }, include: [{
+                        model: User, as: 'Bidder', attributes: { exclude: ['createdAt', 'updatedAt', 'password', 'email', 'admin'] }
+                    }]
+                });
+
+                doc.text('Auction Biddings', { align: 'center' }).moveDown(2);
+
+                const table = new (require('voilab-pdf-table'))(doc, { bottomMargin: 30 });
+                table
+                    // add some plugins (here, a 'fit-to-width' for a column)
+                    .addPlugin(new (require('voilab-pdf-table/plugins/fitcolumn'))({
+                        column: 'name'
+                    })).setColumnsDefaults({
+                        align: 'left'
+                    })
+                    .addColumns([{ id: 'name', header: 'Name', align: 'left' }, { id: 'amount', header: 'Amount', align: 'left' }])
+                const tableData = details.map((detail) => {
+                    return {
+                        name: detail.Bidder.username,
+                        amount: detail.bid_amount
+                    };
+                });
+
+                table.addBody(tableData);
+                doc.moveDown();
 
                 doc.end();
 
@@ -257,9 +394,9 @@ module.exports = class AuctionController {
                     doc.on('end', resolve);
                 });
 
-                return res.status(200).json({ 'status': 'success', 'message': 'pdf generated successfully' });
+                return res.status(200).json({ 'status': 'success', 'message': 'Pdf created successfully' });
             } else {
-                return res.status(400).json({ 'status': 'failed', 'message': 'Item Unsold' });
+                return res.status(400).json({ 'status': 'failed', 'message': 'ERROR' });
             }
         } catch (err) {
             return res.status(500).json({ 'status': 'failed', 'message': err.message });
