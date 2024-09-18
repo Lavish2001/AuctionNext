7d965cfa8b59b64cb6fe2ee75129554e0151fe4a
Lavish Neoliya <128676472+Lavish2001@users.noreply.github.com>
2024-09-18 12:04:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,3 +100,136 @@ module.exports.handler = serverless(app);
 //   return a.salary > b.salary ? -1 : 1
 // });
 // console.log(arr)
+
+
+    // async getArticleViewAnalytic(req, res) {
+
+    //     let input = req.body
+    //     let user_id = req.authUser.user_id;
+    //     // Validate params
+    //     let result = validateParameters(["project_uid","filter"], input);
+    //     if (result != 'valid') {
+    //         let error = formatJoiError(result.errors);
+    //         return res.status(400).send({
+    //             type: "RXERROR",
+    //             message: "Invalid params",
+    //             errors: error
+    //         });
+    //     }
+    //     // Check if the project exists
+    //     let projectData = await getProjectData(input.project_uid);
+    //     if (!projectData) {
+    //         return res.status(400).send({
+    //             type: "RXERROR",
+    //             message: "Invalid project"
+    //         });
+    //     }
+    //     let project_id = projectData.id;
+    //     const permission = await userPrivilege({ type: 'project', searchParam: { user_id: user_id, project_id: project_id }, allowedRole: ['owner','editor'], key: input.project_uid })
+    //     console.log("permission", permission);
+    //     if (permission !== 'valid') {
+    //         return res.status(400).send({
+    //             type: "RXERROR",
+    //             message: permission.message,
+    //         })
+    //     };
+
+    //     let filter = input.filter;
+    //     let array = [];
+    //     let upperAttribute;
+    //     let lowerAttribute
+    //     let current_date;
+    //     let upperWhere;
+    //     let lowerWhere;
+    //     let searchLimit;
+    
+    //     // set values for days filter
+    //     if (filter == 'days') {
+    //       array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    //       upperAttribute = "MONTH",
+    //       lowerAttribute = "DAY",
+    //       upperWhere = "MONTH",
+    //       lowerWhere = "DAY"
+    //       current_date = new Date().getDate();
+    //       searchLimit = 1;
+    //     }
+    
+    //     // set values for months filter
+    //     if (filter == 'months') {
+    //       array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    //       upperAttribute = "YEAR",
+    //       lowerAttribute = "MONTH",
+    //       upperWhere = "YEAR",
+    //       lowerWhere = "MONTH"
+    //       current_date = new Date().getMonth() + 1;
+    //       searchLimit = 1;
+    //     }
+    
+    //     // set values for years filter
+    //     if (filter == 'years') {
+    //       array = [0, 0, 0, 0, 0];
+    //       upperAttribute = "YEAR",
+    //       lowerAttribute = "YEAR",
+    //       upperWhere = "YEAR",
+    //       lowerWhere = "MONTH"
+    //       current_date = new Date().getFullYear();
+    //       searchLimit = 5;
+    //     }
+    
+    //     try{
+    //     // get records form users table based on values
+    //     let article_view = await ChatbotHelpdeskArticleView.findAll({
+    //       attributes: [
+    //         [Sequelize.fn(`${upperAttribute}`, Sequelize.col("created_at")), "u"],
+    //         [Sequelize.fn(`${lowerAttribute}`, Sequelize.col("created_at")), "l"],
+    //         [Sequelize.fn("COUNT", Sequelize.col("created_at")), "count"]
+    //       ],
+    //       where: {
+    //         created_at: {
+    //           [Op.gte]: Sequelize.literal(`NOW() + INTERVAL 1 ${lowerWhere} - INTERVAL ${searchLimit} ${upperWhere} `)
+    //         }
+    //       },
+    //       group: ["u", "l"],
+    //     });
+    //     let data = await this.formatData(article_view,current_date,array);
+    //         return res.status(200).send({ type: "RXSUCCESS", message: "Articles analytics fetch successfully", data: data });  
+    //     } catch(error) {
+    //         console.log("Error", error.message)
+    //         return res.status(400).send({ type: "RXERROR", message: "Something went wrong" });
+    //     }
+    // };
+
+
+    // async formatData(response,current_date,data){
+    //   console.log(JSON.stringify(response))
+    //   let array=data;
+    //   for (let i = 0; i < response.length; i++) {
+    //     // console.log(JSON.stringify(response[i]))
+  
+    //     // index to be updated
+    //     let updateIndex = current_date - response[i].dataValues.l
+  
+    //     // check if index is positive
+    //     if (updateIndex >= 0) {
+    //       array[updateIndex] = +response[i].dataValues.count;
+    //     }
+  
+    //     //check if index is negative
+    //     if (updateIndex < 0) {
+  
+    //       // convert negative index to positive
+    //       let toUpdate = updateIndex * -1
+  
+    //       // logic to update negaive indexs from back side
+    //       let len = array.length
+  
+    //       // find index to be updated for negative values
+    //       let toUpdateIndex = len - toUpdate
+  
+    //       // update to respective
+    //       array[toUpdateIndex] = +response[i].dataValues.count;
+    //     }
+    //   }
+    //   // console.log("\n\n\n\n");
+    //   return array;
+    // };
